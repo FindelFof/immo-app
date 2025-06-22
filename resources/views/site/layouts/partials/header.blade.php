419d9054ea82f8a647fcd010a630f37b7ebf871b
JoCode-dev <cedricduakon@gmail.com>
2025-06-22 13:56:09 +0000

    Update header layout with new navigation links and add mobile header functionality for improved user experience.

--- resources/views/site/layouts/partials/header.blade.php
+++ resources/views/site/layouts/partials/header.blade.php
@@ -10,10 +10,10 @@
                         <div class="main-menu">
                             <ul>
                                 <li>
-                                    <a href="#">Accueil</a>
+                                    <a href="/">Accueil</a>
                                 </li>
-                                <li><a href="/a-propos">Location/Vente</a></li>
-                                <li><a href="/a-propos">Achat de terrain</a></li>
+                                <li><a href="/location-vente">Location/Vente</a></li>
+                                <li><a href="/achat-terrain">Achat de terrain</a></li>
                                 <li><a href="/contact">Contact</a></li>
                             </ul>
                         </div>
@@ -21,7 +21,7 @@
                             <!-- <div class="search-icon header__search header-search-btn">
                                 <a href="#"><img src="assets/img/icons/search1.svg" alt="Rechercher"></a>
                             </div> -->
-                            <a href="/proprietes" class="header-btn1">Connexion / Inscription</a>
+                            <a href="/profil" class="header-btn1">Connexion / Inscription</a>
                         </div>
 
                         <div class="header-search-form-wrapper">
@@ -36,4 +36,39 @@
 </header>
 
 <!-- Header Mobile -->
+ <!--===== MOBILE HEADER STARTS =======-->
+ <div class="mobile-header mobile-haeder1 d-block d-lg-none">
+  <div class="container-fluid">
+    <div class="col-12">
+      <div class="mobile-header-elements">
+        <div class="mobile-logo">
+          <a href="/"><img src="assets/img/logo/logo-dark.png" alt=""></a>
+        </div>
+        <div class="mobile-nav-icon dots-menu">
+          <i class="fa-solid fa-bars-staggered"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="mobile-sidebar mobile-sidebar1">
+  <div class="logosicon-area">
+    <div class="logos">
+      <img src="assets/img/logo/logo-dark.png" alt="">
+    </div>
+    <div class="menu-close">
+      <i class="fa-solid fa-xmark"></i>
+    </div>
+   </div>
+  <div class="mobile-nav mobile-nav1">
+    <ul class="mobile-nav-list nav-list1">
+        <li><a href="/">Accueil</a></li>
+        <li><a href="/location-vente">Location/Vente</a></li>
+        <li><a href="/achat-terrain">Achat de terrain</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+     </div>
+</div>
+<!--===== MOBILE HEADER STARTS =======-->
 @include('site.layouts.partials.mobile-header')
